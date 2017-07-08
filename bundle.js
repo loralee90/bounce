e402316beafbb3a900ee7a85719fbb55cced9022
Lora Lee <loraalee@gmail.com>
2017-07-08 11:39:36 -0400

    Implement difficulty and lives

--- bundle.js
+++ bundle.js
@@ -83,15 +83,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const game = new __WEBPACK_IMPORTED_MODULE_0__game__["a" /* default */]();
   new __WEBPACK_IMPORTED_MODULE_1__game_view__["a" /* default */](game, ctx).start();
-
-  // function animate() {
-  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //   game.draw(ctx);
-  //   game.start();
-  //   requestAnimationFrame(animate);
-  // }
-  //
-  // animate();
 });
 
 
@@ -143,7 +134,7 @@ Paddle.COLOR = "#36454F";
 
 "use strict";
 const COLORS = ["red", "orange", "brown", "green", "blue", "purple", "black"];
-const Y_VELS = [-10, -9, -8];
+const Y_VELS = [-10, -8, -7];
 
 class Ball {
   constructor() {
@@ -170,6 +161,8 @@ class Ball {
 
   bounce() {
     this.dy = Y_VELS[Math.floor(Math.random() * 3)];
+    // this.dy = Math.random() * -10;
+    // this.dy = Math.random() * (-10 + 7) - 7;
     const time = (this.dy * -1) / this.gravity;
     this.dx = 200 / (2 * time);
   }
@@ -186,6 +179,10 @@ class Ball {
     this.x += this.dx;
     this.dy += this.gravity;
     this.y += this.dy;
+  }
+
+  dropped() {
+    return this.y > 560 && this.x < 600;
   }
 }
 
@@ -206,15 +203,21 @@ class Game {
   constructor() {
     this.paddle = new __WEBPACK_IMPORTED_MODULE_1__paddle__["a" /* default */]();
     this.balls = [new __WEBPACK_IMPORTED_MODULE_0__ball__["a" /* default */]()];
-    this.intervalTime = 4700;
+    this.counter = 260;
     this.difficulty = 0;
+    this.lives = 3;
 
-    this.addBalls = this.addBalls.bind(this);
-    setInterval(this.addBalls, 4700);
+    this.addBall = this.addBall.bind(this);
+    this.increaseDifficulty = this.increaseDifficulty.bind(this);
+    this.resetCounter = this.resetCounter.bind(this);
   }
 
-  addBalls() {
-    this.balls.push(new __WEBPACK_IMPORTED_MODULE_0__ball__["a" /* default */]());
+  addBall() {
+    let i = Math.ceil(Math.random() * 2);
+
+    for (let j = 0; j < i; j++) {
+      this.balls.push(new __WEBPACK_IMPORTED_MODULE_0__ball__["a" /* default */]());
+    }
   }
 
   checkCollision() {
@@ -226,7 +229,94 @@ class Game {
   }
 
   increaseDifficulty() {
+    if (this.difficulty < 5) {
+      this.difficulty++;
+    }
+  }
 
+  resetCounter() {
+    if (this.counter > 0) {
+      this.counter--;
+    } else if (this.counter === 0) {
+      this.increaseDifficulty();
+      this.addBall();
+
+      // this.counter = Math.floor(Math.random() * (240 - 100) + 100);
+      // console.log(this.counter)
+
+      // switch (this.difficulty) {
+      //   case 1:
+      //     this.counter = 240;
+      //     break;
+      //   case 2:
+      //     this.counter = 230;
+      //     break;
+      //   case 3:
+      //     this.counter = 210;
+      //     break;
+      //   case 4:
+      //     this.counter = 190;
+      //     break;
+      //   case 5:
+      //     this.counter = 180;
+      //     break;
+      //   case 6:
+      //     this.counter = 160;
+      //     break;
+      //   case 7:
+      //     this.counter = 150;
+      //   case 8:
+      //     this.counter = 140;
+      //     break;
+      //   case 9:
+      //     this.counter = 130;
+      //     break;
+      //   case 10:
+      //     this.counter = 120;
+      //     break;
+      // }
+      //
+      // switch (this.difficulty) {
+      //   case 1:
+      //   case 2:
+      //     this.counter = Math.floor(Math.random() * (260 - 250) + 250);
+      //     break;
+      //   case 3:
+      //   case 4:
+      //     this.counter = Math.floor(Math.random() * (200 - 180) + 180);
+      //     break;
+      //   case 5:
+      //   case 6:
+      //     this.counter = Math.floor(Math.random() * (170 - 150) + 150);
+      //     break;
+      //   case 7:
+      //   case 8:
+      //     this.counter = Math.floor(Math.random() * (150 - 141) + 141);
+      //     break;
+      //   case 9:
+      //   case 10:
+      //     this.counter = 140;
+      //     break;
+      // }
+
+      switch (this.difficulty) {
+        case 1:
+          this.counter = 260;
+          break;
+        case 2:
+          this.counter = 220;
+          break;
+        case 3:
+          this.counter = 200;
+          break;
+        case 4:
+          this.counter = 170;
+          break;
+        case 5:
+          this.counter = 150;
+          break;
+      }
+    }
   }
 
   draw(ctx) {
@@ -238,13 +328,22 @@ class Game {
     this.balls.forEach(ball => ball.draw(ctx));
   }
 
-  step(delta) {
-    console.log(this.balls.length);
-    this.balls.forEach(ball => ball.move());
+  step() {
+    let droppedBalls = [];
+
+    this.balls.forEach((ball, i) => {
+      ball.move();
+      if (ball.dropped()) droppedBalls.push(i);
+    });
+
+    droppedBalls.forEach(i => {
+      this.balls.splice(i, 1);
+      this.lives--;
+    });
+
     this.checkCollision();
-    if (this.difficulty === 1) {
-      this.intervalTime = 1000;
-    }
+    this.resetCounter();
+    console.log(this.lives);
   }
 }
 
@@ -270,22 +369,12 @@ class GameView {
   }
 
   start() {
-    this.lastTime = 0;
     requestAnimationFrame(this.animate.bind(this));
   }
 
-  animate(time) {
-    const timeDelta = time - this.lastTime;
-
-    if (timeDelta > 100) {
-      this.game.difficulty += 1;
-    }
-
-    this.lastTime = time;
-
-    this.game.step(timeDelta);
+  animate() {
+    this.game.step();
     this.game.draw(this.ctx);
-    this.lastTime = time;
 
     requestAnimationFrame(this.animate.bind(this));
   }
